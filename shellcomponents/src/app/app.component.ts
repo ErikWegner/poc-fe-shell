881843cdae2a640056d12bfee4ad3f139e67967b
Erik Wegner <E_Wegner@web.de>
2021-10-21 19:13:28 +0200

    App

--- shellcomponents/src/app/app.component.ts
+++ shellcomponents/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { ShellFooterComponent } from './components/shell-footer/shell-footer.component';
+import { ShellHeaderComponent } from './components/shell-header/shell-header.component';
+import { ShellNavigationComponent } from './components/shell-navigation/shell-navigation.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shellcomponents';
+  constructor(injector: Injector) {
+    const shellHeaderElement = createCustomElement(ShellHeaderComponent, { injector });
+    customElements.define('shell-header', shellHeaderElement);
+    const shellNavigationElement = createCustomElement(ShellNavigationComponent, { injector });
+    customElements.define('shell-navigation', shellNavigationElement);
+    const shellFooterElement = createCustomElement(ShellFooterComponent, { injector });
+    customElements.define('shell-footer', shellFooterElement);
+  }
 }
