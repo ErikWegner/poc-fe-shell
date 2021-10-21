e84a98469ebd8a512d822d4550d90bad081a2a89
Erik Wegner <E_Wegner@web.de>
2021-10-21 18:59:02 +0200

    Components

--- shellcomponents/src/app/app.module.ts
+++ shellcomponents/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShellHeaderComponent } from './components/shell-header/shell-header.component';
+import { ShellNavigationComponent } from './components/shell-navigation/shell-navigation.component';
+import { ShellFooterComponent } from './components/shell-footer/shell-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellHeaderComponent,
+    ShellNavigationComponent,
+    ShellFooterComponent
   ],
   imports: [
     BrowserModule
---
+++ shellcomponents/src/app/components/shell-navigation/shell-navigation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShellNavigationComponent } from './shell-navigation.component';
+
+describe('ShellNavigationComponent', () => {
+  let component: ShellNavigationComponent;
+  let fixture: ComponentFixture<ShellNavigationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShellNavigationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShellNavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
